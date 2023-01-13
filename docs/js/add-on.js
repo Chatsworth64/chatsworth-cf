03212bfc7804659acf9e01004d4831c5ea93599e
Andy Upperton <andrew.upperton@outlook.com>
2023-01-13 21:43:04 +0000

    Dark Mode

--- docs/js/add-on.js
+++ docs/js/add-on.js
@@ -4,3 +4,30 @@
   is an official part of the theme (ex. Pull Requests) should be included in main.js
   and follow the formatting and style given.
 */
+
+const darkIconClass = 'fas fa-moon fa-2x'
+const lightIconClass = 'fas fa-sun fa-2x'
+var darkCSS = $("#dark-theme");
+var darkToggleIcon = $("#dark-toggle i");
+
+// Set theme with the given mode and toggle the theme icon button
+function setThemeMode(mode) {
+  if (mode === "dark") {
+    darkCSS.prop('disabled', false)
+    $("#dark-toggle i").attr('class', lightIconClass)
+  } else if (mode === "light") {
+    darkCSS.prop('disabled', true)
+    darkToggleIcon.attr('class', darkIconClass)
+  }
+}
+
+// Toggle dark mode when user click on the icon button
+$("#dark-toggle").click(function () {
+  if (darkToggleIcon.attr("class") == darkIconClass) {
+    setThemeMode("dark")
+  } else if (darkToggleIcon.attr("class") == lightIconClass) {
+    setThemeMode("light")
+  }
+});
+
+setThemeMode(localStorage.getItem("dark-mode-theme") || "light");
